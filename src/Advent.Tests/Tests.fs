b5a86346ef991b7e29e0c453b474c50244fd5815
Andrew Morger <asmorger@live.com>
2022-12-05 17:08:04 -0600

    Solves Day 5, part 1

--- src/Advent.Tests/Tests.fs
+++ src/Advent.Tests/Tests.fs
@@ -3,6 +3,7 @@ namespace Tests
 open System
 open Advent.Domain
 open Advent.SectionAssignments
+open Advent.Cargo
 open Xunit
 open FsUnit.Xunit
 
@@ -109,3 +110,48 @@ move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2
 """
+(*
+  [<Fact>]
+  let ``Parse crate list`` () =
+    let source = input.Split("\n\n")
+    
+    let crates = parseInitialCrates source[0] |> Seq.toList
+    
+    crates.Length |> should equal 3
+    crates.Head |> should equal (CrateStack [ Crate 'Z'; Crate 'N'  ])
+    crates[1] |> should equal (CrateStack [ Crate 'M'; Crate 'C'; Crate 'D'  ])
+    crates[2] |> should equal (CrateStack [ Crate 'P'  ])
+*)
+  
+  [<Fact>]
+  let ``Pares instruction`` () =
+    let input = "move 1 from 2 to 1\n"
+    let instruction = parseInstruction input
+    
+    Assert.NotNull instruction
+  
+  [<Fact>]
+  let ``Parses instruction lines`` () =
+    let source = input.Split("\n\n") |> Array.last
+    let values = source.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+   
+    let instructions =
+      values
+      |> Array.map parseInstruction
+      
+    instructions.Length |> should equal 4
+
+  [<Fact>]
+  let ``Parses procedure`` () =
+    let procedure = parseProcedure input
+    let stacks, instructions = procedure
+    
+    stacks.Length |> should equal 3
+    instructions.Length |> should equal 4
+    
+  [<Fact>]
+  let ``Run procedure`` () =
+    let procedure = parseProcedure input
+    let result = executeProcedure procedure
+    
+    result |> should equalSeq [|  Crate 'C'; Crate 'M'; Crate 'Z'  |]
---
+++ src/Advent/Cargo.fs
@@ -0,0 +1,132 @@
+module Advent.Cargo
+
+open System
+open FParsec
+
+type Crate =
+  | Crate of char
+  member x.Value = let (Crate value) = x in value
+
+type CrateStack =
+  | CrateStack of Crate list
+
+  member x.Value = let (CrateStack value) = x in value
+
+  member x.Last =
+    let (CrateStack value) = x in
+    value |> ignore
+    value |> List.last
+
+  member x.DropLast =
+    let (CrateStack value) = x in
+    value |> ignore
+    let newValues = value |> List.removeAt (value.Length - 1)
+    CrateStack newValues
+
+  member x.WithValue(crate: Crate) =
+    let mutable (CrateStack value) = x in
+    value |> ignore
+
+    let newList = [ crate ] |> List.append value
+    CrateStack newList
+
+
+type Instruction =
+  { NumberOfItems: int
+    FromStack: int
+    ToStack: int }
+
+let parseCrate (input: char array) =
+  match input with
+  | item when item[0] = '[' -> Some(Crate item[1])
+  | _ -> None
+
+let parseInitialCrates (input: string) =
+  let allRows =
+    input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+
+  let lastIndex = allRows.Length - 2
+  let rows = allRows[0..lastIndex]
+
+  let crateDefinitions =
+    rows
+    |> Seq.map (fun x -> x.ToCharArray())
+    |> Seq.map (Seq.chunkBySize 4)
+    |> Seq.map (Seq.toArray)
+    |> Seq.toArray
+
+  let columns = crateDefinitions[0].Length - 1
+
+  seq {
+    for i = 0 to columns do
+      crateDefinitions
+      |> Array.map (fun x -> x[i])
+      |> Array.map (parseCrate)
+      |> Array.filter Option.isSome
+      |> Array.map Option.get
+      |> Array.rev
+      |> Array.toList
+      |> CrateStack
+  }
+
+let createInstruction moves fromStack toStack =
+  { NumberOfItems = moves
+    FromStack = fromStack - 1
+    ToStack = toStack - 1 }
+
+let parseInstruction (input: string) =
+  let int_ws = pint32 .>> spaces
+  let str_ws s = pstring s .>> spaces
+
+  let moves = str_ws "move" >>. int_ws
+  let fromStack = str_ws "from" >>. int_ws
+  let toStack = str_ws "to" >>. int_ws
+
+  let instruction = pipe3 moves fromStack toStack createInstruction
+  let parser = run instruction input
+
+  match parser with
+  | Success (v, _, _) -> v
+  | Failure (msg, _, _) -> failwith msg
+
+let parseProcedure (input: string) =
+  let source = input.Split("\n\n")
+  let startingStackDrawing = source[0]
+  let instructionsSource = source[1]
+
+  let stacks = parseInitialCrates startingStackDrawing |> Seq.toArray
+
+  let instructions =
+    instructionsSource.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map parseInstruction
+
+  (stacks, instructions)
+
+let runInstruction (stacks: CrateStack[]) (instruction: Instruction) =
+  let mutable finalStacks = stacks
+  let mutable fromStack = finalStacks[instruction.FromStack]
+  let mutable toStack = finalStacks[instruction.ToStack]
+
+  for i = 1 to instruction.NumberOfItems do
+    let crate = fromStack.Value |> List.last
+    let newFrom = fromStack.DropLast
+    let newTo = toStack.WithValue crate
+
+    finalStacks[instruction.FromStack] <- newFrom
+    finalStacks[instruction.ToStack] <- newTo
+
+    fromStack <- newFrom
+    toStack <- newTo
+
+    printf "something"
+
+  finalStacks
+
+let executeProcedure (procedure: CrateStack[] * Instruction[]) =
+  let mutable stacks, instructions = procedure
+
+  for instruction in instructions do
+    let result = runInstruction stacks instruction
+    stacks <- result
+
+  stacks |> Array.map (fun x -> x.Last)
